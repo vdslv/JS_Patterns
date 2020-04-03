f5821d441e5fb6d122b39e34229bcf286ac1928c
Дмитро Михайлюк <dimamykhaylukgmail.com@MacBook-Pro-Dmitro.local>
2020-04-04 00:50:22 +0300

    create repo structure

---
+++ CREATIONAL/Factory_method/Factory_method.ts
@@ -0,0 +1,53 @@
+
+interface Product {
+  operation(): string;
+}
+
+abstract class Creator {
+  
+  public abstract factoryMethod(): Product;
+
+  public someOperation(): string {
+      // Call the factory method to create a Product object.
+      const product = this.factoryMethod();
+      // Now, use the product.
+      return `Creator: The same creator's code has just worked with ${product.operation()}`;
+  }
+}
+
+class ConcreteCreator1 extends Creator {
+  public factoryMethod(): Product {
+      return new ConcreteProduct1();
+  }
+}
+
+class ConcreteCreator2 extends Creator {
+  public factoryMethod(): Product {
+      return new ConcreteProduct2();
+  }
+}
+
+class ConcreteProduct1 implements Product {
+  public operation(): string {
+      return '{Result of the ConcreteProduct1}';
+  }
+}
+class ConcreteProduct2 implements Product {
+  public operation(): string {
+      return '{Result of the ConcreteProduct2}';
+  }
+}
+
+function clientCode(creator: Creator) {
+  // ...
+  console.log('Client: I\'m not aware of the creator\'s class, but it still works.');
+  console.log(creator.someOperation());
+  // ...
+}
+
+console.log('App: Launched with the ConcreteCreator1.');
+clientCode(new ConcreteCreator1());
+console.log('');
+
+console.log('App: Launched with the ConcreteCreator2.');
+clientCode(new ConcreteCreator2());
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-Design Patterns
+                        DESIGN PATTERNS
+
+STRUCTURAL:
+  ·Adapter
+  ·Bridge
+  ·Composite
+  ·Decorator
+  ·Facade
+  ·Flyweight
+  ·Proxy
+
+BEHAVIORAL:
+  ·Chain-of-responsibility
+  ·Command
+  ·Iterator
+  ·Mediator
+  ·Memento
+  ·Observer
+  ·State
+  ·Strategy
+  ·Template
+  ·Visitor
+
+CREATIONAL:
+  ·Abstract-factory
+  ·Builder
+  ·Factory-method
+  ·Prototype
+  ·Singleton
